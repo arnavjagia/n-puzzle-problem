acca8a37e31248bff4f8e597e0e4cd3c0a0c8882
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-04-23 05:05:29 +0530

    Random states generating

--- ui/src/Landing/landing.jsx
+++ ui/src/Landing/landing.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Grid from "../components/Grid/grid";
 import "./landing.css";
+import axios from "axios";
 
 function Landing() {
   const [gridSize, setGridSize] = useState(3); // Initial grid size, e.g., 3x3 grid
+  const [gridCells, setGridCells] = useState([]);
+
+  
+  useEffect(() => {
+    const sendGridState = async () => {
+      try {
+        const response = await axios.get(`/generate/${gridSize}`);
+        const { initialGrid } = response.data;
+        console.log("New grid state:", initialGrid);
+        console.log(typeof initialGrid);
+        setGridCells(initialGrid);
+      } catch (error) {
+        console.error("Error sending grid state:", error);
+      }
+    };
+    sendGridState();
+  }, [gridSize])
+
   return (
     <>
       <landing-wrapper>
-        <p>Landing page: {gridSize}</p>
-        <Grid gridsize={gridSize} updateGridSize={setGridSize} />
-        <Grid gridsize={gridSize} updateGridSize={setGridSize} />
+        <Grid gridsize={gridSize} updateGridSize={setGridSize} gridCells={gridCells} updateGridCells={setGridCells} />
+        <Grid gridsize={gridSize} updateGridSize={setGridSize} gridCells={gridCells} updateGridCells={setGridCells} />
       </landing-wrapper>
     </>
   );
