48da168836b986d71d23e49d71c8872c4452db7b
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-04-23 06:29:51 +0530

    Revert "Set axios baseURL"
    This reverts commit def850fbee37ed1299a130e566af2ba9e38b6543.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from random import shuffle
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
@@ -22,12 +21,6 @@ app.add_middleware(
 @app.get("/api/health")
 async def read_root() -> dict:
     return {"message": "API is up and running!"}
-
-@app.get("/api/grid/{size}")
-async def get_initial_grid(size: int) -> dict:
-    a = [i for i in range(size*size)]
-    shuffle(a)
-    return {"initialGrid": a}
 
 @app.post("/api/grid")
 async def update_grid(request: Request):
