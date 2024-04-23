05157321b962b2a1eb00cff4e8204f8762cae6db
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-04-23 06:29:51 +0530

    Revert "Update backend structure"
    This reverts commit 74539ab0341f66155144781f268dee4ddb44f27c.

---
+++ backend/app/api.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "https://localhost:5173",
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your todo list."}
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return { "data": todos }
