bf697b01dfc5fd39129bb7a6100a982c19060c52
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-04-23 06:29:51 +0530

    Revert "Generate random states"
    This reverts commit 40544bf0f414c5e8beddd78c36261dddbc6db44e.

--- ui/src/Landing/landing.jsx
+++ ui/src/Landing/landing.jsx
@@ -5,61 +5,31 @@ import axios from "axios";
 
 function Landing() {
   const [gridSize, setGridSize] = useState(3); // Initial grid size, e.g., 3x3 grid
-  const [initialGridCells, setInitialGridCells] = useState([]);
-  const [goalGridCells, setGoalGridCells] = useState([]);
+  const [gridCells, setGridCells] = useState([]);
 
-  const initGridState = async () => {
-    try {
-      const response = await axios.get(`/generate/${gridSize}`);
-      const { initialGrid } = response.data;
-      const { goalGrid } = response.data;
-      console.log("Initial grid state:", initialGrid);
-      console.log("Goal grid state:", goalGrid);
-      setInitialGridCells(initialGrid);
-      setGoalGridCells(goalGrid);
-    } catch (error) {
-      console.error("Error sending grid state:", error);
-    }
-  };
-
+  
   useEffect(() => {
-    initGridState();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [gridSize]);
-
-  // Function to handle slider change and update grid size
-  const handleSliderChange = (event) => {
-    const newSize = parseInt(event.target.value, 10);
-    setGridSize(newSize);
-    setInitialGridCells([]);
-    setGoalGridCells([]);
-  };
+    const sendGridState = async () => {
+      try {
+        const response = await axios.get(`/generate/${gridSize}`);
+        const { initialGrid } = response.data;
+        console.log("New grid state:", initialGrid);
+        console.log(typeof initialGrid);
+        setGridCells(initialGrid);
+      } catch (error) {
+        console.error("Error sending grid state:", error);
+      }
+    };
+    sendGridState();
+  }, [gridSize])
 
   return (
-    <super-landing-wrapper>
-      <input
-        type="range"
-        min="2"
-        max="4"
-        value={gridSize}
-        onChange={handleSliderChange}
-      />
+    <>
       <landing-wrapper>
-        {/* Slider to control grid size */}
-        <Grid
-          gridsize={gridSize}
-          updateGridSize={setGridSize}
-          gridCells={initialGridCells}
-          updateGridCells={setInitialGridCells}
-        />
-        <Grid
-          gridsize={gridSize}
-          updateGridSize={setGridSize}
-          gridCells={goalGridCells}
-          updateGridCells={setGoalGridCells}
-        />
+        <Grid gridsize={gridSize} updateGridSize={setGridSize} gridCells={gridCells} updateGridCells={setGridCells} />
+        <Grid gridsize={gridSize} updateGridSize={setGridSize} gridCells={gridCells} updateGridCells={setGridCells} />
       </landing-wrapper>
-    </super-landing-wrapper>
+    </>
   );
 }
 
